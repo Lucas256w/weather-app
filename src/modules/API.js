486d7166fe55001dd81aa5445137ddcadc5d84bc
Jiahao Huamani <lucashuamani256w@gmail.com>
2023-10-28 23:32:29 -0400

    Implement loading screen.

--- src/modules/API.js
+++ src/modules/API.js
@@ -4,28 +4,42 @@ let data;
 
 class OpenWeatherApi {
   static async nameToCoords(cityName, currentMetric) {
-    const response = await fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=4fa3f0720ceb21b2300ab7ae8ff03f66`,
-    );
-
-    const coorddata = await response.json();
-    if (coorddata.length !== 0) {
-      OpenWeatherApi.latAndLonToWeather(
-        coorddata[0].lat,
-        coorddata[0].lon,
-        cityName,
-        currentMetric,
+    const loadingScreen = document.getElementById("loading-screen");
+    loadingScreen.style.display = "flex";
+    try {
+      const response = await fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=4fa3f0720ceb21b2300ab7ae8ff03f66`,
       );
+
+      const coorddata = await response.json();
+      if (coorddata.length !== 0) {
+        OpenWeatherApi.latAndLonToWeather(
+          coorddata[0].lat,
+          coorddata[0].lon,
+          cityName,
+          currentMetric,
+        );
+      }
+    } catch (error) {
+      console.log(error);
     }
   }
 
   static async latAndLonToWeather(lat, lon, cityName, currentMetric) {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=${currentMetric}&exclude=minutely,alerts&appid=4fa3f0720ceb21b2300ab7ae8ff03f66`,
-    );
+    const loadingScreen = document.getElementById("loading-screen");
 
-    data = await response.json();
-    DisplayStats.updateDisplay(data, cityName, currentMetric);
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=${currentMetric}&exclude=minutely,alerts&appid=4fa3f0720ceb21b2300ab7ae8ff03f66`,
+      );
+
+      data = await response.json();
+      DisplayStats.updateDisplay(data, cityName, currentMetric);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      loadingScreen.style.display = "none";
+    }
   }
 }
 
